b1761f15fe0d3bb5b7861441b9b107595ba1e973
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-05-09 12:14:10 +0530

    added typdefs

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -5,6 +5,9 @@
 // import { typeDefs } from "@/graphql/schema";
 // import { resolvers } from "@/graphql/resolvers";
 
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+
 // export type Context = {
 // 	prisma: PrismaClient;
 // };
@@ -14,3 +17,40 @@
 // export default startServerAndCreateNextHandler(apolloServer, {
 // 	context: async (req, res) => ({ req, res, prisma }),
 // });
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const typeDefs = `#graphql
+type Novel {
+  id: ID!
+  title: String
+  image: String
+  createdAt: String
+  updatedAt: String
+  author: [Author]
+}
+
+type Author {
+  id: String
+  name: String
+  novelId: String
+}
+type Query {
+    novels: [Novel]
+}
+
+type Mutation{
+  
+}
+`;
+
+const server = new ApolloServer({
+    resolvers,
+    typeDefs
+})
+
+export default startServerAndCreateNextHandler(server)
