422f88b12bf10ce4569429a796624d12ede6fe46
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-06-17 17:02:30 +0530

    nil

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -14,6 +14,7 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { PrismaClient } from "@prisma/client";
 import { prisma } from "../../prisma/db";
 
+
 export type Context = {
   prisma: PrismaClient;
 };
